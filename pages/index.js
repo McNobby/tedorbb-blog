2d137b6e31bc5d5e52483d7aafdc58d22d41379e
McNobby <76711087+McNobby@users.noreply.github.com>
2021-11-01 19:34:54 +0100

    resturcturing and added projects

--- pages/index.js
+++ pages/index.js
@@ -3,16 +3,27 @@ import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 import Link from 'next/link'
 
+
 export default function Home() {
   return (
    <div className="Mainpage">
      <h1>Teodor Berntsen Bøe</h1>
      <div className="pagePanel">
+
      <Link href="/interesser">
        <div className={styles.pageBtn}>
+         <Image src="/heart.svg" width={50} height={50} />
           <a>Interesser</a>
         </div>
        </Link>
+
+       <Link href="/prosjekter">
+       <div className={styles.pageBtn}>
+         <Image src="/folder.svg" width={50} height={50} />
+          <a>Prosjekter</a>
+        </div>
+       </Link>
+
      </div>
    </div>
   )
---
+++ pages/prosjekter/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Prosjekter = () => {
+    return (
+        <div>
+            <h1>Prosjekter</h1>
+        </div>
+    )
+}
+
+export default Prosjekter
