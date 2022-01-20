e62774713c860e86b02c4892336dfe66c3c3ccbd
McNobby <76711087+McNobby@users.noreply.github.com>
2022-01-20 18:24:18 +0100

    added animations

--- components/ArticleThumbnails.jsx
+++ components/ArticleThumbnails.jsx
@@ -1,9 +1,39 @@
 import React, { useEffect, useState } from "react";
 import styles from "../styles/Home.module.scss";
-
+import { motion } from "framer-motion";
 import Link from "next/link";
 import getCollection from "../lib/getCollection";
-import { collection } from "firebase/firestore";
+
+const variants = {
+    hidden: { y: 100 },
+    show: {
+        y: 0,
+        transition: {
+            staggerChildren: 1,
+        },
+    },
+    exit: {
+        opacity: 0,
+        scale: 1.3,
+        transition: {
+            duration: 0.2,
+        },
+    },
+};
+
+const item = {
+    hidden: {
+        y: 100,
+    },
+    show: {
+        y: 0,
+        transition: {
+            type: "spring",
+            mass: 0.5,
+            damping: 9.2,
+        },
+    },
+};
 
 const ArticleThumbnail = ({ site }) => {
     const [data, setData] = useState([]);
@@ -19,16 +49,28 @@ const ArticleThumbnail = ({ site }) => {
 
     const list = data.map((e) => {
         return (
-            <Link href={`/${site}/${e.id}`} key={e.id}>
-                <div className={styles.gridElement}>
-                    <h1>{e.data.title}</h1>
-                    <p>{e.data.thumbnail}</p>
-                </div>
-            </Link>
+            <motion.div variants={item}>
+                <Link href={`/${site}/${e.id}`} key={e.id}>
+                    <div className={styles.gridElement}>
+                        <h1>{e.data.title}</h1>
+                        <p>{e.data.thumbnail}</p>
+                    </div>
+                </Link>
+            </motion.div>
         );
     });
 
-    return <div className={styles.gridContainer}>{list}</div>;
+    return (
+        <div
+            className={styles.gridContainer}
+            variants={variants}
+            inital="hidden"
+            animate="show"
+            exit="exit"
+        >
+            {list}
+        </div>
+    );
 };
 
 export default ArticleThumbnail;
