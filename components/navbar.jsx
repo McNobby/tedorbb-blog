025d71a34d51e0d8b87fe53dff89aadfcae1b913
McNobby <76711087+McNobby@users.noreply.github.com>
2022-01-20 17:43:19 +0100

    prettified

--- components/navbar.jsx
+++ components/navbar.jsx
@@ -14,7 +14,6 @@ const Navbar = () => {
                 //user is logged in
                 setLoggedIn(true);
             } else {
-                //do nothing atm
                 setLoggedIn(false);
             }
         });
