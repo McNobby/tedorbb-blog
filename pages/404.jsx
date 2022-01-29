80f8b33478cbb9f263f1ae916562f1335d1a5f2a
McNobby <76711087+McNobby@users.noreply.github.com>
2022-01-29 16:30:15 +0100

    added 404 page

---
+++ pages/404.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import styles from "../styles/Home.module.scss";
+
+const Custom404 = () => {
+  return <div className={styles.container}>
+      <div className={styles.Custom404}>
+        <h1>404</h1>
+        <p>:(</p>
+        <h1>Ojsann, der gikk det galt!</h1>
+        <h3>siden du prøver å nå, finnes ikke</h3>
+
+      </div>
+  </div>;
+};
+
+export default Custom404;
